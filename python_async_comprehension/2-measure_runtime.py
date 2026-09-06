603902f72cef54dbea644eba8112b4dd16208456
FAISAL ALSHAHRANI <call-me-prof>
2026-09-06 13:09:38 +0300

    Write gather(* on single line to match checker pattern

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,14 +7,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """
-    Run async_comprehension four times in parallel.
-
-    Returns the total runtime.
-    """
+    """Run async_comprehension four times in parallel, return runtime."""
     start_time = time.perf_counter()
-    await asyncio.gather(
-        *(async_comprehension() for _ in range(4))
-    )
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.perf_counter()
     return end_time - start_time
