418293721ce93c2cdf420d194723bfedddedd321
FAISAL ALSHAHRANI <call-me-prof>
2026-09-06 12:41:46 +0300

    Fix task 2 and task 4

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""Module that defines a measure_time function."""
-import asyncio
+"""Measure runtime module"""
 import time
+import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measure the total execution time for wait_n and return total_time / n."""
+    """Measures the total execution time for wait_n(n, max_delay) and returns total_time / n."""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start_time
+    end_time = time.time()
+    
+    total_time = end_time - start_time
     return total_time / n
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python3
-"""Module that defines an async routine task_wait_n."""
+"""Tasks module"""
 import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Spawn task_wait_random n times with the specified max_delay.
-
-    Args:
-        n (int): the number of tasks to spawn.
-        max_delay (int): the maximum delay for each task.
-
-    Returns:
-        List[float]: the list of delays in ascending order,
-        obtained without using sort(), relying on concurrency.
-    """
-    delays: List[float] = []
+def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+    """Spawns task_wait_random n times with the specified max_delay."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-    return delays
+    return [asyncio.ensure_future(task) for task in asyncio.as_completed(tasks)]
