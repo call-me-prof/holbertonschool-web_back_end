de929ef0720e5e22f18280e373e2587ab26b5eaf
FAISAL ALSHAHRANI <call-me-prof>
2026-09-06 12:59:00 +0300

    Add tasks 0-2: python_async_comprehension

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module that defines a measure_runtime coroutine."""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run async_comprehension four times in parallel and return the runtime."""
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    end_time = time.perf_counter()
+    return end_time - start_time
